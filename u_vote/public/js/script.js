f2c7628393ab9897f6e01d4b499c455b211971fe
Stefan Hartmann <stefanhartmann@gmail.com>
2016-09-05 10:54:46 -0400

    create register view & create header in participation view

--- u_vote/public/js/script.js
+++ u_vote/public/js/script.js
@@ -2,15 +2,18 @@
 (function(){
 console.log("js loaded")
 
-var email = $('#email-input').val()
-var status_buttons = $('.status-buttons')
+var email = $('#email-input').val();
+var status_buttons = $('.status-buttons');
 
 $('#email-input').on("click", function(){
   if (this.value === "Ex: homer.simpson@springfielduni.edu") {
        this.value = '';
-   }
-   var buttons = $('.status-buttons')
-   buttons.css("opacity", "1")
+       $(this).css("opacity", "1")
+   };
+   var buttons = $('.status-buttons');
+   buttons.css("opacity", "1");
+   var status = $("#status-instruction");
+   status.show();
 })
 
 })();
