e99574531977a2f1d5e37bdb0d093db96095a4de
Stefan Hartmann <stefanhartmann@gmail.com>
2016-09-16 10:35:37 -0400

    update db config for heroku

--- config/environments.rb
+++ config/environments.rb
@@ -2,7 +2,7 @@ require 'zlib'
 
 
 configure :production, :development do
-	db = URI.parse('postgres://dasboogaloo@localhost:5432/schools_db')
+	db = URI.parse(ENV['DATABASE_URL'])
 
 	ActiveRecord::Base.establish_connection(
 			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
