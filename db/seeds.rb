c3f759d3ff0465e18bbcc070c18cd31f800d587e
Stefan Hartmann <stefanhartmann@gmail.com>
2016-09-17 18:49:24 -0400

    update db/seed target

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,21 @@ require 'faker'
 
 School.destroy_all
 
-CSV.foreach("./UniDataSanitized.csv", {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+CSV.foreach("./UniData2015Sanitized.csv", {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
   School.create(row.to_hash)
 
 end
 
-status = ["Am I Registered?", "I've Registered", "I Need to Register"]
-
-
-20.times do
-  u1 = User.create(
-    email: Faker::Internet.email,
-    domain: Faker::Internet.domain_name,
-    status: status[rand(3)]
-    )
-end
+# status = ["Am I Registered?", "I've Registered", "I Need to Register"]
+#
+#
+# 20.times do
+#   u1 = User.create(
+#     email: Faker::Internet.email,
+#     domain: Faker::Internet.domain_name,
+#     status: status[rand(3)]
+#     )
+# end
 
 puts "Created #{School.count} schools"
 puts "Created #{User.count} users"
