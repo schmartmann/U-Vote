4a8a7c63ecf5855f11b5cf0501df94a749f902d7
Stefan Hartmann <stefanhartmann@gmail.com>
2016-10-02 21:59:56 -0400

    try to fix school.rb NoMethodError

--- models/school.rb
+++ models/school.rb
@@ -1,11 +1,8 @@
 class School < ActiveRecord::Base
 
   def self.search(search)
-    where("instnm ilike ?", "%#{search}%")
-    .or(where("stabbr ilike ?", "%#{search}%"))
+    where("instnm ilike ?", "%#{search}%").or(where("stabbr ilike ?", "%#{search}%"))
     .or(where("webaddr ilike ?", "%#{search}%"))
     .or(where("countynm ilike ?", "%#{search}%"))
   end
 end
-
-# .or(where("zip = ?", "#{search.to_i}"))
