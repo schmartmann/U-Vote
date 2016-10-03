11bed9eef1db0f20895f7bf72fa0d578ff7e5ee3
Stefan Hartmann <stefanhartmann@gmail.com>
2016-10-03 11:37:33 -0400

    retry fix search bar

--- models/school.rb
+++ models/school.rb
@@ -2,9 +2,10 @@ class School < ActiveRecord::Base
 
   def self.search(search)
     # where("instnm ilike ? OR stabbr ilike ? OR webaddr ilike ? OR countynm ilike?", "%#{search}%")
-    where("instnm ilike ?", "%#{search}%")
+    # where("instnm ilike ?", "%#{search}%")
     # .or(where("stabbr ilike ?", "%#{search}%"))
     # .or(where("webaddr ilike ?", "%#{search}%"))
     # .or(where("countynm ilike ?", "%#{search}%"))
+    where("countynm ilike ?", "%#{search}%")
   end
 end
